8dadc733f9677e65e3322be602abd5dc330f27b4
Nova.XOTTIC <nate56242@gmail.com>
2026-02-07 01:42:17 -0500

    Update menu.Rayfield.lua

--- Rayfield/menu/menu/menu.Rayfield.lua
+++ Rayfield/menu/menu/menu.Rayfield.lua
@@ -510,7 +510,7 @@ function RayfieldLibrary:Notify(NotificationSettings)
 			game:GetService("Debris"):AddItem(script,0)
 		end
 
-		Notification.Actions.Template.Visible = false
+		Notification.Actions.Template.Visible = true
 
 		if NotificationSettings.Actions then
 			for _, Action in pairs(NotificationSettings.Actions) do
