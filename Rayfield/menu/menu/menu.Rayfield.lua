feb9e06efacfa45bc653e73f50a6259ead7c2eb7
Nova.XOTTIC <nate56242@gmail.com>
2026-02-07 10:28:00 -0500

    Update menu.Rayfield.lua

--- Rayfield/menu/menu/menu.Rayfield.lua
+++ Rayfield/menu/menu/menu.Rayfield.lua
@@ -114,7 +114,7 @@ local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
 
 -- Interface Management
-local Rayfield = game:GetObjects("rbxassetid://84934427046886")[1]
+local Rayfield = game:GetObjects("rbxassetid://99639913744279")[1]
 
 Rayfield.Enabled = false
 
